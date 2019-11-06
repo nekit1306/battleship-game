234b6aa91fb06959ac0887301dd0c021816bdc2c
nikita <nekit1306@gmail.com>
2019-11-06 14:41:08 -0800

    space fix

--- app/containers/App.js
+++ app/containers/App.js
@@ -24,7 +24,6 @@ class App extends Component<Props> {
 
     componentDidMount() {
         const socket = io('http://localhost:3000');
-
         this.props.updateSocket(socket);
         this.props.loadInitialEvents(socket);
     }
